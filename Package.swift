499813b217cf26fbd9b798930bd7c43a679f38e1
Berrie Kremers <33800564+katoemba@users.noreply.github.com>
2023-08-08 14:45:11 +0200

    Update Package.swift
    Removed superfluous comment about build version.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUPnP",
-    // Change the macOS version to .v13 to compile UPnPCodeGenerator
     platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
